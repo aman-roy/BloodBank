b80b37fde1527896daf67ee570a6560ffa48f8d0
Aman Roy <royaman8757@gmail.com>
2018-07-19 13:01:49 +0530

    added struct donor and struct acceptor

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+struct donor {
+    char first_name[20];
+    char middle_name[20];
+    char last_name[20];
+    int age;
+    char nationality[3];
+    char address[100];
+    char phone_number[10];
+    char blood_group[5];
+    char sex;
+    char date[10];
+    char time[10];
+};
+
+struct acceptor {
+    struct donor info;
+    char hospital[50];
+    char hospital_address[100];
+};
+
+
 int main()
 {
     printf("Hello world!\n");
